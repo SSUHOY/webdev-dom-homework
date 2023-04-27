5d6a63165e8db666d563fa4c5ff8305aa97cb0ea
SSUHOY <sammysoul96@gmail.com>
2023-04-27 20:57:10 +0300

    Стилизует форму входа/регистрации

--- components/login-components.js
+++ components/login-components.js
@@ -8,53 +8,47 @@ export function renderLoginComponent({ appEl, setToken, fetchRenderComments, com
     <div class="form">
     <h3 class="form-title">Форма ${isLoginMode ? "входа" : "регистрации"}</h3>
         <div class="form-row">
-        ${
-          isLoginMode ? '' : ' <input type="text" id="name-input" class="input" placeholder = Имя> <br>'          
-        }
+        ${isLoginMode ? '' : ' <input type="text" id="name-input" class="userinput" placeholder = Имя> <br>'
+      }
+        <input type="text" id="login-input" class="userinput" placeholder = 'Логин'>
         <br>
-        <input type="text" id="login-input" class="input" placeholder = 'Логин'>
-        <br>
-        <br>
-        <input type="password" id="password-input" class="input" placeholder = 'Пароль'>
+        <input type="password" id="password-input" class="userinput" placeholder = 'Пароль'>
         </div>
-        <br>
-        <br>
         <button class="button" id='login-button'>${isLoginMode ? "Войти" : "Зарегистрироваться"}</button>
-        <br> <br> <br>
-        <button class="button" id='toggle-button'>Перейти ${isLoginMode ? "к регистрации" : "ко входу"}</button> 
+        <button class="toggle" id='toggle-button'>Перейти ${isLoginMode ? "к регистрации" : "ко входу"}</button> 
         </div>  
     `
-appEl.innerHTML = appHtml;
+    appEl.innerHTML = appHtml;
 
-document.getElementById('login-button').addEventListener('click', () => {
-const login = document.getElementById('login-input').value;
-const password = document.getElementById('login-input').value;
+    document.getElementById('login-button').addEventListener('click', () => {
+      const login = document.getElementById('login-input').value;
+      const password = document.getElementById('login-input').value;
 
-if (!login) {
-  alert('введите логин')
-  return
-}
+      if (!login) {
+        alert('введите логин')
+        return
+      }
 
-if (!password) {
-  alert('введите пароль')
-  return
-}
-loginUser({
-  login: 'admin',
-  password: 'admin',
-}).then((user) => {
-  setToken(`Bearer ${user.user.token}`);
-  fetchRenderComments(comments, 'Bearer bgc0b8awbwas6g5g5k5o5s5w606g37w3cc3bo3b83k39s3co3c83c03ck');
-}).catch((error) => {
-  alert(error.message);
-});
-});
+      if (!password) {
+        alert('введите пароль')
+        return
+      }
+      loginUser({
+        login: 'admin',
+        password: 'admin',
+      }).then((user) => {
+        setToken(`Bearer ${user.user.token}`);
+        fetchRenderComments(comments, 'Bearer bgc0b8awbwas6g5g5k5o5s5w606g37w3cc3bo3b83k39s3co3c83c03ck');
+      }).catch((error) => {
+        alert(error.message);
+      });
+    });
 
-document.getElementById('toggle-button').addEventListener('click', () => {
-isLoginMode = !isLoginMode;
-renderForm();
-});
+    document.getElementById('toggle-button').addEventListener('click', () => {
+      isLoginMode = !isLoginMode;
+      renderForm();
+    });
   }
-   renderForm();
-  
+  renderForm();
+
 }
